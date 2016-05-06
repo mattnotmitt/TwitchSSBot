070e3f14adf60108e75487b23babef4b9979c0af
Matt C <matthew@artemisbot.co.uk>
2016-05-06 23:49:12 +0100

    Create ircPull.js

---
+++ ircPull.js
@@ -0,0 +1,31 @@
+var irc = require("tmi.js");
+var fs = require("fs")
+var options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        username: "onscoinbot",
+        password: "oauth:xxxxxxxxxxx"
+    },
+    channels: ["#onscreenlol"]
+};
+
+var client = new irc.client(options);
+
+// Connect the client to the server..
+client.connect();
+client.on("subanniversary", function (channel, username, months) {
+    console.log(username);
+		fs.appendFile('subs.txt', username + "\n", function (err) {
+		});
+});
+client.on("subscription", function (channel, username) {
+    console.log(username);
+		fs.appendFile('subs.txt', username + "\n", function (err) {
+		});
+});
